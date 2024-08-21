907d190de7c45b4f0352faa90c03a98366aa2a53
Titicagens <willianweber123@gmail.com>
2024-08-21 20:52:53 -0300

    Criação do Projeto Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer idadePessoa = 18;
+        String nomePessoa = "Willian";
+
+        /*
+         *  > maior
+         * >= maior igual
+         *  < menor
+         * <= menor igual
+         * == é igual
+         * != é diferente
+         * equal compara texto
+         * */
+
+        if(idadePessoa > 21){
+           System.out.println("é maior que 21!");
+        }else{
+            System.out.println("Não é maior");
+        }
     }
 }
