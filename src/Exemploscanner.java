3b66899c46f9bdb530a663794b1f8e2fd995778c
Titicagens <willianweber123@gmail.com>
2024-08-28 19:53:16 -0300

    Criação do Projeto Java

---
+++ src/Exemploscanner.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Exemploscanner {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String nome =  "";
+        Integer idade = 0;
+        Double peso = 0.0;
+
+
+        System.out.println("Escreva seu nome:");
+        nome = sc.next();
+
+        System.out.println("Informe sua idade:");
+        idade = sc.nextInt();
+
+        System.out.println("Informe sua peso:");
+        peso = sc.nextDouble();
+
+        System.out.println("Seu nome é : " + nome);
+
+        System.out.println("Seu peso é : " + peso);
+
+        System.out.println("Sua idade é: " + idade);
+
+    }
+}
+
